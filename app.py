c3148b15e6f11084e4ecfc9db0a0ac32fd3999ee
FC Lutfi Dev <muhammad.lutfi@flash-coffee.com>
2021-12-19 16:19:45 +0700

    Update Index Comming Soon

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return '<h1>I want to Deploy Flask</h1>'
+  return '<h1>Coin Insight Comming Soon</h1>'
 
 if __name__ == "__main__":
      app.debug = False
-     port = int(os.environ.get('PORT', 33507))
+     port = int(os.environ.get('PORT', 3355))
      waitress.serve(app, port=port)
