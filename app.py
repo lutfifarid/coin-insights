9da5a3bc4ee7c2306c4214d62574d7bd0f73f8c0
FC Lutfi Dev <muhammad.lutfi@flash-coffee.com>
2021-12-19 15:26:03 +0700

    Testing procfile heroku using waiters

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
- 
+from flask import Flask, Response, url_for
+from waitress import serve
+
 app = Flask(__name__)
- 
+# app.run(debug=True)
+
 @app.route('/')
 def index():
-  return '<h1>I want to Deploy Flask to Heroku</h1>'
+  return '<h1>I want to Deploy Flask to Herokss22 asdasasdasd asu</h1>'
+
+serve(app, listen='*:8080')
